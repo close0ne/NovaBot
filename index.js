308a515cd08491a13e4a1f6a39175891c0369374
close0ne <turtleland@protonmail.com>
2020-01-14 12:10:39 +0000

    House Of The Rising Sun

--- index.js
+++ index.js
@@ -1,21 +1,24 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const { token, prefix } = require('./auth.json');
+const { token, prefix } = require('./auth/auth.json');
 const sheet = require('./read.js');
 
 client.on('message', message => {
   if (!message.content.startsWith(prefix) || message.author.bot) return;
   const args = message.content.slice(prefix.length).split(' ');
   const command = args.shift().toLowerCase();
-  const name = args[0].charAt(0).toUpperCase() + args[0].slice(1);
 
     if (command === 'balance') {
       if (!args.length) {
         return message.channel.send(`${message.author} - Treba zadat meno - !balance pepega`);
       }
 
-      console.log(sheet.balance(name));
+      const name = args[0].charAt(0).toUpperCase() + args[0].slice(1);
+      const balance = sheet.balance(name);
 
+      const meno = balance[0];
+      const pending = balance[2];
+      const paid = balance[3];
 
     } else {
             return message.channel.send(`${message.author} - Ty si dobra pepega`);
